22bab8c2b604bb0e61062ae0036fd91b05c06e93
Cody Morris <codyc.morris@gmail.com>
2021-07-27 18:43:16 -0400

    work

--- 02-Homework/Instructions/starter_code/api_keys.py
+++ 02-Homework/Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "5b4ee063faf4f35390ed5d155ca118c9"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAUUovgNj4qwir3DwfyVEDtpVsXvhfir5Q"
